0f46b976ef1e898005c361265d453cd5b5018874
EnzooKms <ekemmas@edenschool.fr>
2023-03-10 14:40:43 +0100

    add table database

---
+++ database/migrations/1678450982920_posts.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'posts'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title')
+      table.string('thumbnail')
+      table.string('description')
+      table.integer('username')
+      .unsigned()
+      .references('users.id')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
